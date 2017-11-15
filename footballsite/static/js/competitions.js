785f1a873035dfaf98d6b0100898a8595e063d19
Team Hiro <TeamHiro@sky.uk>
2017-11-15 17:05:43 +0000

    Display team logos along with team names in query results

--- footballsite/static/js/competitions.js
+++ footballsite/static/js/competitions.js
@@ -2,81 +2,87 @@ var ws_scheme = window.location.protocol === "https:" ? "wss" : "ws";
 var socket = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/competitions");
 var populator;
 
-socket.onmessage = function(message) {
+socket.onmessage = function (message) {
     var competition = JSON.parse(message.data);
-    $(populator.populate(competition)).appendTo('#competitions');    
+    $(populator(competition)).appendTo('#competitions');
 };
 
 function findCompetitions(document, filter) {
-    populator = new Populator(JSON.parse(filter)['type']);
+    populator = new ResultPopulators()[JSON.parse(filter)['type']];
     socket.onopen = function () {
-        socket.send(filter)
+        socket.send(filter);
     };
-    if (socket.readyState == WebSocket.OPEN) {
-	socket.onopen();
+    if (socket.readyState === WebSocket.OPEN) {
+        socket.onopen();
     }
 }
 
-function Populator(type) {
-    this.type = type;
+function ResultPopulators() {
+    this.populators = {
+        'topbottom': this.topBottom,
+        'underover': this.underOver
+    };
+    return this.populators;
 }
 
-Populator.prototype.populate = function(competition) {
-    if(this.type === 'topbottom') {
-        return this.topBottom(competition);
-    } else if(this.type === 'underover') {
-        return this.underOver(competition);
-    }
-    alert('Unknown filter type ' + this.type);
-};
-
-Populator.prototype.topBottom = function(competition) {
+ResultPopulators.prototype.topBottom = function (competition) {
     var html = '<h3>' + competition.name + '</h3>' +
         '<table class="u-full-width">' +
-	'<thead>' +
-	'<tr>' +
-	'<th>Time</th>' +
-	'<th/>' +
-	'<th>Home Team</th>' +
-	'<th/>' +
-	'<th>Away Team</th>' +
-	'</tr>' +
-	'</thead>' +
-	'<tbody>';
-    $.each(competition.matches, function(index, value) { 
-	html += '<tr>' +
-		'<td>' + value.datetime + '</td>' +
-		'<td><img class="u-max-full-width" src=\"' + value.homeTeam.crestUrl + '\"/></td>' +
-		'<td>' + value.homeTeam.name + '(' + value.homeTeam.standing + ')</td>' +
-		'<td><img class="u-max-full-width" src=\"' + value.awayTeam.crestUrl + '\"/></td>' +
-		'<td>' + value.awayTeam.name + '(' + value.awayTeam.standing + ')</td>' +
-                '</tr>'
+        '<thead>' +
+        '<tr>' +
+        '<th>Time</th>' +
+        '<th/>' +
+        '<th>Home Team</th>' +
+        '<th/>' +
+        '<th>Away Team</th>' +
+        '</tr>' +
+        '</thead>' +
+        '<tbody>';
+
+    function getTeamHtml(team) {
+        return '<td>' + (team.crestUrl ? '<img class="resize" src=\"' + team.crestUrl + '\"/>' : '') + '</td>' +
+            '<td>' + team.name + ' (' + team.standing + ')</td>';
+    }
+
+    $.each(competition.matches, function (index, value) {
+        html += '<tr>' +
+            '<td>' + value.datetime + '</td>' +
+            getTeamHtml(value.homeTeam) +
+            getTeamHtml(value.awayTeam);
     });
     html += '</tbody></table>';
     return html;
 };
 
-Populator.prototype.underOver = function(competition) {
+ResultPopulators.prototype.underOver = function (competition) {
     var html = '<h3>' + competition.name + '</h3>' +
         '<table class=\"u-full-width\">' +
-	'<thead>' +
-	'<tr>' +
-	'<th>Time</th>' +
-	'<th>Home Team</th>' +
-	'<th>Away Team</th>' +
+        '<thead>' +
+        '<tr>' +
+        '<th>Time</th>' +
+        '<th/>' +
+        '<th>Home Team</th>' +
+        '<th/>' +
+        '<th>Away Team</th>' +
         '<th>Under or Equal Chance</th>' +
         '<th>Over or Equal Chance</th>' +
-	'</tr>' +
-	'</thead>' +
-	'<tbody>';
-    $.each(competition.matches, function(index, value) { 
-	html += '<tr>' +
-		'<td>' + value.datetime + '</td>' +
-		'<td>' + value.homeTeam + '</td>' +
-		'<td>' + value.awayTeam + '</td>' +
- 		'<td>' + value.stats.under_or_equal_chance + '</td>' +
- 		'<td>' + value.stats.over_or_equal_chance + '</td>' +
-		'</tr>'
+        '</tr>' +
+        '</thead>' +
+        '<tbody>';
+
+    function getTeamHtml(team) {
+        return '<td>' + (team.crestUrl ? '<img class="resize" src=\"' + team.crestUrl + '\"/>' : '') + '</td>' +
+            '<td>' + team.name + '</td>';
+    }
+
+    $.each(competition.matches, function (index, value) {
+        html += '<tr>' +
+            '<td>' + value.datetime + '</td>' +
+            getTeamHtml(value.homeTeam) +
+            getTeamHtml(value.awayTeam) +
+            '<td>' + value.stats.under_or_equal_chance + '</td>' +
+            '<td>' + value.stats.over_or_equal_chance + '</td>' +
+            '</tr>'
     });
     html += '</tbody></table>';
     return html;
--- matches/consumers.py
+++ matches/consumers.py
@@ -1,6 +1,6 @@
 import json
-import uuid
 import logging
+import uuid
 from channels import Group
 from channels.sessions import channel_session
 from matchfinder.filter import filtermatch
@@ -10,38 +10,39 @@ competitions = footballapi.get_competitions()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-@channel_session
-def ws_connect(message):
-	sessionId = str(uuid.uuid4())
-	logger.info('connect sessionId: ' + sessionId)
-	message.reply_channel.send({"accept": True})
-	Group('competitions-' + sessionId).add(message.reply_channel)
-	message.channel_session['sessionId'] = sessionId
 
 @channel_session
-def ws_receive(message):	
-	logger.info('receive message for sessionId: ' + message.channel_session['sessionId'])
-	publish_competitions(message.channel_session['sessionId'], json.loads(message['text']))
+def ws_connect(message):
+    session_id = str(uuid.uuid4())
+    logger.info('connect sessionId: ' + session_id)
+    message.reply_channel.send({"accept": True})
+    Group('competitions-' + session_id).add(message.reply_channel)
+    message.channel_session['sessionId'] = session_id
+
+
+@channel_session
+def ws_receive(message):
+    logger.info('receive message for sessionId: ' + message.channel_session['sessionId'])
+    publish_competitions(message.channel_session['sessionId'], json.loads(message['text']))
+
 
 @channel_session
 def ws_disconnect(message):
-	sessionId = message.channel_session['sessionId']
-	logger.info('disconnect sessionId: ' + sessionId)
-	Group('competitions-' + sessionId).discard(message.reply_channel)
+    session_id = message.channel_session['sessionId']
+    logger.info('disconnect sessionId: ' + session_id)
+    Group('competitions-' + session_id).discard(message.reply_channel)
 
-def publish_competition(sessionId, competition, query):
-	c = filtermatch.get_matches(competition, query)
-	Group('competitions-' + sessionId).send({'text': json.dumps({
-		'name': c.name,
-		'matches': [{
-			'datetime': match.datetime,
-			'homeTeam': match.homeTeam.__dict__,
-			'awayTeam': match.awayTeam.__dict__
-		} for match in c.matches] })
-	}, immediately=True)
 
-def publish_competitions(sessionId, query):
-	selected_competitions = [competition for competition in competitions if competition['league'] == query['league']] \
-		if query['league'] != 'All' else competitions
+def publish_competition(session_id, competition, query):
+    c = filtermatch.get_matches(competition, query)
+    Group('competitions-' + session_id).send({'text': json.dumps({
+        'name': c.name,
+        'matches': [dict(match) for match in c.matches]})
+    }, immediately=True)
 
-	list(map(lambda competition: publish_competition(sessionId, competition, query), selected_competitions))
+
+def publish_competitions(session_id, query):
+    selected_competitions = [competition for competition in competitions if competition['league'] == query['league']] \
+        if query['league'] != 'All' else competitions
+
+    list(map(lambda competition: publish_competition(session_id, competition, query), selected_competitions))
